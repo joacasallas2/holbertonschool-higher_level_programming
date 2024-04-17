8f3743489297699357d6cc6052ca42ba5632e43e
joacasallas2 <proyecta.ds@gmail.com>
2024-04-17 11:09:40 -0500

    docstring

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+# Author - Joana Casallas
+
+"""program that prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for i in range(97, 123):
     print("{}".format(chr(i)), end="")
--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Author - Joana Casallas
+
+"""program that prints the ASCII alphabet, in lowercase, not followed by a new line."""
 for i in range(97, 123):
     if (chr(i) == "q" or chr(i) == "e"):
         continue
