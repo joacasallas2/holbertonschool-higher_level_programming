ff816015c87dafa9d98e321ae38599acf5186c21
joacasallas2 <proyecta.ds@gmail.com>
2024-04-17 12:28:20 -0500

    islower:  function that checks for lowercase character.

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+def islower(c):
+    """function that checks for lowercase character."""
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
