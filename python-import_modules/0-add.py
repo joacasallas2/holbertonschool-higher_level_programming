81432826390b8934257217398e28e7d99c9a258e
joacasallas2 <proyecta.ds@gmail.com>
2024-04-19 13:21:29 -0500

    Import a simple function from a simple file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""
+program that imports the function def add(a, b):
+from the file add_0.py
+and prints the result of the addition 1 + 2 = 3
+"""
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
