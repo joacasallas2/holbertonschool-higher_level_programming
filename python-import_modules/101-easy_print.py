15c1e1b437f2adff626bfeb96e67140103743921
joacasallas2 <proyecta.ds@gmail.com>
2024-04-22 12:42:32 -0500

    program that prints #pythoniscool, followed by a new line, in the standard output.

--- python-import_modules/101-easy_print.py
+++ python-import_modules/101-easy_print.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-# Author: Joana Casallas
-"""
-program that prints #pythoniscool, followed by a new line,
-in the standard output.
-"""
-if __name__ == "__main__":
-    __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
