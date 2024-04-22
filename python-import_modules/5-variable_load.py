40e55d9ca56760f3bbb7683a6434c3e4d15f96fd
joacasallas2 <proyecta.ds@gmail.com>
2024-04-22 04:55:01 -0500

    program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""
+program that imports the variable a from the file
+variable_load_5.py and prints its value.
+"""
+if __name__ == "__main__":
+    import variable_load_5
+    print("{}".format(variable_load_5.a))
