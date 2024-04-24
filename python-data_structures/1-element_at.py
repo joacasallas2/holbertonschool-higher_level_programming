0704d7229ac1cec95148e81168f351f5c0c33f55
joacasallas2 <proyecta.ds@gmail.com>
2024-04-24 10:58:25 -0500

    def print_list_integer(my_list=[]): function that prints all integers of a list.

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+def element_at(my_list, idx):
+    """retrieve an element from a list.
+    Keyword arguments:
+    my_list -- The list
+    idx -- The index
+    If idx is negative or if idx is out of range the function return None
+    """
+    lenght = len(my_list)
+    if idx < 0 or idx > lenght - 1:
+        return None
+    for i in range(0, lenght):
+        if i == idx:
+            return my_list[i]
+    
