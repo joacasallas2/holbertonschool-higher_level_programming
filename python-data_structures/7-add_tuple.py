10b1a221409ded74b57e80b4da8b276b54b7686b
joacasallas2 <proyecta.ds@gmail.com>
2024-04-25 18:35:07 -0500

    def add_tuple(tuple_a=(), tuple_b=()):  function that adds 2 tuples.

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    function that adds 2 tuples.
+    Keywords arguments:
+    tuple_a -- first tuple
+    tuple_b -- second tuple
+    Returns a tuple with 2 integers
+    """
+    tuple_c = (0, 0)
+    if len(tuple_a) < 2:
+        tuple_a += tuple_c
+    if len(tuple_b) < 2:
+        tuple_b += tuple_c
+
+    list_values = [tuple_a[0], tuple_b[0], tuple_a[1], tuple_b[1]]
+    values = [0 if i is None else i for i in list_values]
+    return ((values[0] + values[1]), (values[2] + values[3]))
