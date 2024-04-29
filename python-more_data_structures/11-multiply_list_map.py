551a2da5816e35ff30ed137d96e6b72a914f8ca8
joacasallas2 <proyecta.ds@gmail.com>
2024-04-29 17:14:27 -0500

    def multiply_list_map(my_list=[], number=0): function that returns a list with all values multiplied by a number without using any loops.

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
 def multiply_list_map(my_list=[], number=0):
-    """
-    return a list with all values multiplied
-    by a number without using any loops.
-    """
     new_list = list(map(lambda x: x * number, my_list))
     return new_list
