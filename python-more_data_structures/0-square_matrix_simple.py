c31ffad8a1d0475e76631fc6120f92407335c7c8
joacasallas2 <proyecta.ds@gmail.com>
2024-04-30 13:40:50 -0500

    improve 0-square_matrix_simple.py task

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     """computes the square value of all integers of a matrix"""
     new_matrix = []
-    for i in range(len(matrix)):
-        new_matrix.append([j * j for j in matrix[i]])
+    for i in matrix:
+        new_matrix.append([j * j for j in i])
     return new_matrix
