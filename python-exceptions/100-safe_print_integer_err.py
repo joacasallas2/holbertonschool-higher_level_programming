173786e60699f1e7328bdd25aafb3e2ba277d764
joacasallas2 <proyecta.ds@gmail.com>
2024-05-03 17:55:47 -0500

    def safe_print_integer_err(value): function that prints an integer.

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,10 +5,9 @@ import sys
 
 def safe_print_integer_err(value):
     """print an integer."""
-    exc_type, exc_obj, tb = sys.exc_info()
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
-        print("Exception: {}".format(exc_obj), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
