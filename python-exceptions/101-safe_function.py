ad0e2c17e546ec0b0fbae286e545744e1194a870
joacasallas2 <proyecta.ds@gmail.com>
2024-05-03 18:07:29 -0500

    safe_function(fct, *args): function that executes a function safely.

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+import sys
+
+
+def safe_function(fct, *args):
+    """execute a function safely."""
+    try:
+        return fct(args[0], args[1])
+    except (ZeroDivisionError, IndexError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None
