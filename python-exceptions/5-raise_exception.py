54843664f3ec8a8e2cbd6ced4cce4134a651ba4c
joacasallas2 <proyecta.ds@gmail.com>
2024-05-03 15:54:10 -0500

    def raise_exception(): raises a type exception.

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+def raise_exception():
+    """raise a type exception."""
+    try:
+        raise Exception
+    except Exception:
+        print("Exception raised")
