0d39d8cc1b77681c61d38983090de9dcc931e152
joacasallas2 <proyecta.ds@gmail.com>
2024-05-10 18:43:09 -0500

    class Square that defines a square adding Private instance attribute: position

---
+++ python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""Define a Class Square"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data
+        Args:
+            size (int): The size of the square
+            position (int): The position of the square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position of the square"""
+        if isinstance(value, tuple) is False or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """return the area of the square"""
+        return self.size ** 2
+
+    def my_print(self):
+        """print in stdout the square with the character #"""
+        if self.size == 0:
+            print(end="\n")
+        for i in range(self.position[1]):
+            print(end="\n")
+        for i in range(self.size):
+            for j in range(self.position[0]):
+                print("_", end="")
+            for j in range(self.size):
+                print("#", end="")
+            print(end="\n")
