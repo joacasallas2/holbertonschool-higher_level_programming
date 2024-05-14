5adca2e52c4ac03878c4926081a2c89d713dd806
joacasallas2 <proyecta.ds@gmail.com>
2024-05-14 13:11:44 -0500

    class Square that defines a square with __str__ method

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -62,14 +62,11 @@ class Square:
 
     def __str__(self):
         """print in stdout the square with the character #"""
-        if self.size == 0:
+        for i in range(self.position[1]):
             print(end="\n")
-        else:
-            for i in range(self.position[1]):
+        for i in range(self.size):
+            [print(" ", end="") for j in range(self.position[0])]
+            [print("#", end="") for j in range(self.size)]
+            if i < self.size - 1:
                 print(end="\n")
-            for i in range(self.size):
-                [print(" ", end="") for j in range(self.position[0])]
-                [print("#", end="") for j in range(self.size)]
-                if i < self.size - 1:
-                    print(end="\n")
         return ""
