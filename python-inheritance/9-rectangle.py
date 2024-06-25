b0c297b8e02207b5845bb04426e01003df0544da
joacasallas2 <proyecta.ds@gmail.com>
2024-06-25 16:21:49 -0500

    instance

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""This module provides a class Rectangle"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Define a rectangle"""
+    def __init__(self, width, height):
+        """Initialize data"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """calculate the area"""
+        return self.__height * self.__width
+
+    def print(self):
+        """print in the stdout"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __str__(self):
+        """print in the stdout"""
+        return f"{self.print()}"
