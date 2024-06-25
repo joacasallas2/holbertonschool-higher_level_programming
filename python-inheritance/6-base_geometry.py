28502c605caa6f6500190acc24a8e4e404ed7be6
joacasallas2 <proyecta.ds@gmail.com>
2024-06-25 09:48:49 -0500

    Public instance method: def area(self):

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""This module provides a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+    def area(self):
+        """implement area"""
+        try:
+            raise Exception
+        except Exception:
+            return "area() is not implemented"
