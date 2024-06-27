0ad0166e9fc0fbdb25d721a9ebc63d9d9d1d9cbd
joacasallas2 <proyecta.ds@gmail.com>
2024-06-27 14:14:23 -0500

    serializing dict in json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
 """This module provides a a function class_to_json(obj)"""
-import json
 
 
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure"""
     dict_obj = obj.__dict__
-    json_str = json.dumps(dict_obj)
-    json_dict = json.loads(json_str)
-    return json_dict
+    return dict_obj
+
