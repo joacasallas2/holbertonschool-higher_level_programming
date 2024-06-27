3f9492e666b9930441bf08b430214bc60720340a
joacasallas2 <proyecta.ds@gmail.com>
2024-06-27 16:04:25 -0500

    reload json

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""This module provides a class Student"""
+
+
+class Student:
+    """ defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialize the data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        dict_student = self.__dict__
+        if attrs is None:
+            return dict_student
+        dict_attrs = {}
+        for key in attrs:
+            if hasattr(self, key):
+                dict_attrs[key] = dict_student[key]
+        return dict_attrs
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        new_attrs = {}
+        self.__dict__ = new_attrs
+        for key in json:
+            new_attrs[key] = json[key]
