f2c7a62797439c9342e8bb72a0a6dea4e5ba56b0
joacasallas2 <proyecta.ds@gmail.com>
2024-06-30 07:23:10 -0500

    tests - 0

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+"""Tests"""
+
+import unittest
+from models.base import Base
+
+
+class TestBaseAssigningID(unittest.TestCase):
+    """group of tests of Base() for assigning an ID"""
+    def test_auto_id_assignemt_automatically(self):
+        """Test of Base() for assigning automatically an ID exists"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_manual_id_assignemt(self):
+        """Test of Base() for assigning an manual ID exists"""
+        b4 = Base(12)
+        b5 = Base(13)
+
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 13)
+
+if __name__ == "__main__":
+    unittest.main()
