edd3b308d404f85686cdb5b8aa3fff69db8cc671
joacasallas2 <proyecta.ds@gmail.com>
2024-07-01 06:36:36 -0500

    tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,16 +7,16 @@ from models.rectangle import Rectangle
 from models.base import Base
 
 
-class TestRectangleAssigningID(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
     """group of tests of Rectangle class"""
     def setUp(self):
         "setup for test cases"
         Base._Base__nb_objects = 0
 
-    def test_auto_id_assignemt_automatically(self):
+    def test_auto_id_assignemt(self):
         """Test of Rectangle() for assigning automatically an ID exists"""
         r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
+        r2 = Rectangle(1, 2)
 
         self.assertEqual(r1.id, 1)
         self.assertEqual(r2.id, 2)
@@ -26,6 +26,18 @@ class TestRectangleAssigningID(unittest.TestCase):
         r3 = Rectangle(10, 2, 0, 0, 12)
 
         self.assertEqual(r3.id, 12)
+
+    def test_x_assignemt(self):
+        """Test of Rectangle class for assigning an x value exists"""
+        r4 = Rectangle(1, 2, 3)
+
+        self.assertEqual(r4.x, 3)
+
+    def test_y_assignemt(self):
+        """Test of Rectangle class for assigning an y value exists"""
+        r5 = Rectangle(1, 2, 3, 4)
+
+        self.assertEqual(r5.y, 4)
 
 if __name__ == "__main__":
     unittest.main()
