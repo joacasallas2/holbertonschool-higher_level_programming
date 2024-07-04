f8a268760d2910c1dc010d4ab87cf987afe20a34
joacasallas2 <proyecta.ds@gmail.com>
2024-07-04 11:21:28 -0500

    def to_json_string(list_dictionaries):

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
 """This module provides a class Base"""
+import json
 
 
 class Base:
@@ -14,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
