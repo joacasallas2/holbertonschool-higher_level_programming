38f85ff5f504d3709f427629b0678bb907fec2b8
joacasallas2 <proyecta.ds@gmail.com>
2024-07-05 11:33:32 -0500

    18. Dictionary to Instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -46,6 +46,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        new_instance = cls(1)
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        else:
+            new_instance = cls(1)
         new_instance.update(**dictionary)
         return new_instance
