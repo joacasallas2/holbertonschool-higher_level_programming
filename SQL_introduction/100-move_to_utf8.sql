db0f9dc50e80dcc4a3fb0b307eb0e8542bc4f181
joana.casallas <joacasallas@gmail.com>
2024-09-05 17:15:17 -0500

    17. Go to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -2,10 +2,10 @@
 USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-CONVERT TO utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
