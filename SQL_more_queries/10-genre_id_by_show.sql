6a28466dfe02a4c47b982dd71a3be68ce63f722e
joana.casallas <joacasallas@gmail.com>
2024-09-08 15:27:38 -0500

    10. Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- Import the database dump from hbtn_0d_tvshows
+SELECT
+    s.title,
+    g.genre_id
+FROM
+    tv_show_genres g
+    LEFT JOIN tv_shows s ON s.id = g.show_id
+ORDER BY
+s.title ASC,
+g.genre_id ASC;
