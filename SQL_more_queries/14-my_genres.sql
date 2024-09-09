6e6286cc8890adf4b19a7dea34b73482953a5d88
joana.casallas <joacasallas@gmail.com>
2024-09-09 12:09:43 -0500

    14. My genres - left join + where

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,6 @@ FROM
     LEFT JOIN tv_show_genres sg ON sg.genre_id = g.id
     LEFT JOIN tv_shows s ON s.id = sg.show_id
 WHERE
-    s.title = 'Dexter';
+    s.title = 'Dexter'
+ORDER BY
+    g.name
