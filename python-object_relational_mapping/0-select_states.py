62697154c8d420f1711d3fa0f9d34cafaf234b24
joana.casallas <joacasallas@gmail.com>
2024-09-30 13:11:46 -0500

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 # Author: Joana Casallas
-""" List all states from the database hbtn_0e_0_usa"""
+""" List all states from the database hbtn_0e_0_usa """
 import MySQLdb
 
-db=MySQLdb.connect(host="localhost", user="root", password="", database="hbtn_0e_0_usa")
+db = MySQLdb.connect(
+    host="localhost",
+    user="root", password="",
+    database="hbtn_0e_0_usa")
 cur = db.cursor()
 query = "SELECT * FROM states ORDER BY id ASC;"
 cur.execute(query)
