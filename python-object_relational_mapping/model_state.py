64db62904a9bbc7c40b4e7328801fa3ee831e9fd
joana.casallas <joacasallas@gmail.com>
2024-10-02 13:39:17 -0500

    6. First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,20 +2,15 @@
 # Author: Joana Casallas
 """ This module contains the class definition of a State
 and an instance Base = declarative_base() """
-import sys
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 Base = declarative_base()
-Base.metadata.create_all(engine)
 
 
 class State(Base):
-    """Class State"""
+    """Class State mapped to the states table"""
     __tablename__ = 'states'
 
     id = Column(Integer(), primary_key=True)
