0a0d0f7cadbf4eddee29158bb575a0b6074dac22
joana.casallas <joacasallas@gmail.com>
2024-10-05 04:42:38 -0500

    16. List relationship

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Author: Joana Casallas
+""" List all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa """
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_and_cities = session.query(State).join(
+        City).order_by(State.id, City.id).all()
+    for state in states_and_cities:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"\t{city.id}: {city.name}")
+
+    session.close()
