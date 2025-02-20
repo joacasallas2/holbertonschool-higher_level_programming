9cc6b128087fd1dd7fe06e4e52210c5916cfed6d
joana.casallas <joacasallas@gmail.com>
2025-02-20 11:57:55 -0500

    4. Rectangle #4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -24,7 +24,6 @@ class Rectangle {
   }
 
   double () {
-    this.height = this.height * 2;
     this.width = this.width * 2;
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
