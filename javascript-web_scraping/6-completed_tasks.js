773c68c0e8b46101a2a9ef89003bb838c83a2ed8
joana.casallas <joacasallas@gmail.com>
2025-02-24 11:52:18 -0500

    6. How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// Compute the number of tasks completed by user id
+const request = require('request');
+const args = process.argv;
+const len = args.length;
+
+if (len < 3) {
+  console.error('Usage must be ./6-completed_tasks.js <url>');
+  process.exit(1);
+}
+const url = args[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('error: ', error);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error fetching url');
+    process.exit(1);
+  }
+  try {
+    const data = JSON.parse(body);
+    const dictUsers = {};
+    for (const obj of data) {
+      if (!(obj.userId in dictUsers)) {
+        dictUsers[obj.userId] = 0;
+      }
+      if (obj.completed === true) {
+        dictUsers[obj.userId] += 1;
+      }
+    }
+    console.log(dictUsers);
+  } catch (parseError) {
+    console.log('error parsing JSON: ', parseError);
+  }
+});
