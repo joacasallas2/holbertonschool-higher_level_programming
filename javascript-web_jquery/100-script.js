95c2ec7d34405d8f79e01052356e4f890044a46d
joana.casallas <joacasallas@gmail.com>
2025-03-03 10:46:32 -0500

    document.evenListener

---
+++ javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+/* Change text color of the header to red */
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('header').style.color = '#FF0000';
+});
