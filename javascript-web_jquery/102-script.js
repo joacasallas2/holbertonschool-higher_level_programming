932ce9026966bc9cb22f551ce8628372a92b3877
joana.casallas <joacasallas@gmail.com>
2025-03-03 13:30:14 -0500

    fix cors

--- javascript-web_jquery/102-script.js
+++ javascript-web_jquery/102-script.js
@@ -2,18 +2,16 @@
 // fetch IP code and return hello in the correct language
 
 $(document).ready(function () {
-  $("#btn_translate").on("click", function (event) {
+  $("#btn_translate").on("click", function () {
     const languageCode = $("#language_code").val();
-
-    $.ajax({
-      url: `https://www.fourtonfish.com/hellosalut/?lang=${languageCode}`,
-      method: "GET",
-      success: function (data) {
-        $("#hello").text(data.hello);
-      },
-      error: function (err) {
-        console.error(err);
-      },
+    console.log(languageCode);
+    const proxyUrl = "https://api.allorigins.win/get?url=";
+    const apiurl = `https://www.fourtonfish.com/hellosalut/?lang=${languageCode}`;
+    $.get(proxyUrl + encodeURIComponent(apiurl), function (data) {
+        const response = JSON.parse(data.contents);
+      $("#hello").text(response.hello);
+    }).fail(function (error) {
+      console.error("Error fetching translation", error);
     });
   });
 });
