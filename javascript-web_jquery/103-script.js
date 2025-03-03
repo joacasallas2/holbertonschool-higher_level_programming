4cef1adc01109baf54ff34dd92cfb11cb661b4b3
joana.casallas <joacasallas@gmail.com>
2025-03-03 13:58:28 -0500

    toggle functions

---
+++ javascript-web_jquery/103-script.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// fetch IP code and return hello in the correct language
+
+$(document).ready(function () {
+  function fetchTranslation() {
+    const languageCode = $("#language_code").val();
+    console.log(languageCode);
+    const proxyUrl = "https://api.allorigins.win/get?url=";
+    const apiurl = `https://www.fourtonfish.com/hellosalut/?lang=${languageCode}`;
+    $.get(proxyUrl + encodeURIComponent(apiurl), function (data) {
+      const response = JSON.parse(data.contents);
+      $("#hello").text(response.hello);
+    }).fail(function (error) {
+      console.error("Error fetching translation", error);
+    });
+  }
+  $("#btn_translate").on("click", fetchTranslation);
+  $("#language_code").on("keypress", function (event) {
+    if (event.key === "Enter") {
+        fetchTranslation();
+    }
+  });
+});
